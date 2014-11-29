26ed90ed54b0519ae9ad7107b61fb9a492c3c744
Joey Figaro <joey@joeyfigaro.com>
2014-11-29 02:06:19 -0500

    improved localstorage functions and loading screen map transition slightly

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,7 @@ define(['jquery', 'underscore', 'svgloader', 'mapbox', 'modernizr'], function($,
     Mapbox.accessToken = members.options.accessToken;
 
     // Show the loading screen
-    var loader = new SVGLoader(members.elements.loader, { speedIn: 400, easingIn: mina.easeinout });
+    var loader = new SVGLoader(members.elements.loader, { speedIn: 1000, easingIn: mina.easeinout });
     loader.show();
 
     // Check for geolocation capabilities
@@ -26,23 +26,35 @@ define(['jquery', 'underscore', 'svgloader', 'mapbox', 'modernizr'], function($,
             layer = Mapbox.tileLayer(members.options.mapID),
             featureLayer = Mapbox.featureLayer().loadURL('../data/flushots.json').addTo(map);
 
+        map.on('load', function() {
+          $(loader.el).hide(); // Hide our loading screen
+          $('.marker-list').show();
+        });
+
         // Build marker menu
         map.featureLayer.on('ready', function(e) {
           var markers = [];
-          this.eachLayer(function(marker) { markers.push(marker); });
+          this.eachLayer(function(marker) {
+            markers.push(marker);
+          });
 
           buildMarkerList(markers);
         });
 
-        // Center the map on the location of the user
-        map.setView([position.coords.latitude, position.coords.longitude], members.options.mapZoomLevel);
+        // Check local storage data from previous sessions
+        // if it isn't available, set it...
+        if(_.isEmpty(localStorage.getItem('latitude')) && _.isEmpty(localStorage.getItem('longitude'))) {
+          map.setView([position.coords.latitude, position.coords.longitude], members.options.mapZoomLevel);
 
-        // Ensure our loading screen is visible long enough
-        // to finish the start animation
-        setTimeout(function() {
-          loader.hide();
-          $('.marker-list').show();
-        }, 1000);
+          saveToLocalStorage({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+        }
+        // ...otherwise, use it
+        else {
+          map.setView([localStorage.getItem('latitude'), localStorage.getItem('longitude')], members.options.mapZoomLevel);
+        }
 
         var marker = L.marker([position.coords.latitude, position.coords.longitude], {
           icon: Mapbox.marker.icon({
@@ -66,6 +78,26 @@ define(['jquery', 'underscore', 'svgloader', 'mapbox', 'modernizr'], function($,
       }, 1000);
     }
   };
+
+  var saveToLocalStorage = function(items) {
+    if(Modernizr.localstorage) {
+      _.each(items, function(value, key, list) {
+        localStorage.setItem(key, JSON.stringify(value));
+      });
+    }
+  }
+
+  function checkLocalStorage() {
+    var archive = [],
+    keys = Object.keys(localStorage),
+    i = 0;
+
+    for (; i < keys.length; i++) {
+      archive.push( localStorage.getItem(keys[i]) );
+    }
+
+    return archive;
+  }
 
   var handleGeolocationErrors = function(error) {
     Mapbox.accessToken = members.options.accessToken;
